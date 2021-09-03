b1e46f4d9cd51f3ec5342e1b7de5805b1ce22ffa
Juan Silva <silvajuandavid.08@gmail.com>
2021-09-02 21:43:39 -0500

    change_base_url

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = "https://distracted-lovelace-471e26.netlify.app/";
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const randomNumber = (min = 0, max = 1) =>
